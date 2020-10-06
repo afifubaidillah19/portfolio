5e632dfa142893b949c52d4ee83b47fc9dad56c5
Afif Ubaidillah <50175881+afifubaidillah19@users.noreply.github.com>
2020-10-06 10:15:33 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+    // setting item permenu
+    let $btns = $('.project-area .button-group button');
+
+    $btns.click(function(e){
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    // setting project image
+    $('.project-area .button-group #btn1').trigger('click');
+
+    // popup image
+    $('.project-area .grid .test-popup-link').magnificPopup({
+      type: 'image',
+      // other options
+      gallery:{enabled:true} 
+    });
+
+    // owl carousel
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+    // Sticky navigation menu
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed(){
+        if ($('.header_area').length){
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                if(scroll >= nav_offset_top){
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                }else{
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }   
+    }
+
+    navbarFixed();
+
+});
